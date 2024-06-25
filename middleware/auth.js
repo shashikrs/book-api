7749ede75a5b5cabf75038402c98678d33d32fd3
Shashikumar Selvaraj <shashikr88.sap@gmail.com>
2024-06-25 18:42:26 +0200

    validated bearer token

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken");
 //middle to verify the access token
 const auth = async (req, res, next) => {
   const bearerToken = req.headers["authorization"];
+
+  if (!bearerToken) res.sendStatus(403);
+
   const token = bearerToken.split(" ")[1];
 
   if (token == null) return res.status(401).json({ message: "Unauthorized" });
