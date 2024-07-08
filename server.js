697d9d367200a2e52b5b0b0667dbd7c8ea665ef5
Shashikumar Selvaraj <shashikr88.sap@gmail.com>
2024-07-08 20:06:07 +0200

    make swagger work on vercel

--- server.js
+++ server.js
@@ -42,7 +42,11 @@ const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use(
   "/api-docs",
   swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL })
+  swaggerUi.setup(swaggerDocs, {
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl: CSS_URL,
+  })
 );
 
 //configure cors
