258744a185de6865c5f21024e778863d70a6dd1b
Shashikumar Selvaraj <shashikr88.sap@gmail.com>
2024-07-08 20:30:02 +0200

    make swagger work on vercel

--- server.js
+++ server.js
@@ -46,7 +46,17 @@ app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocs, {
-    customCssUrl: CSS_URL,
+    customSiteTitle: "Backend Generator",
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
   })
 );
 
